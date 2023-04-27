e3132a39ed8b293a53bcd21c8d4c83d3e657825a
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-04-27 13:54:58 -0300

    Upload exercicios laços de repetição

---
+++ src/ExercicioDois.java
@@ -0,0 +1,11 @@
+public class ExercicioDois {
+    public static void main(String[] args){
+        int num =2, resultado;
+
+        for (int i = 0; i<=10; i++){
+            resultado = num*i;
+            System.out.println("2 x " + i + " = " + resultado);
+        }
+
+    }
+}
